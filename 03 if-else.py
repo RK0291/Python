69b4b1b9498b37839e4fab45635f5b5deccb6306
RK0291 <rajendra1983@gmail.com>
2020-11-17 13:13:06 +0530

    Add files via upload

---
+++ 03 if-else.py
@@ -0,0 +1,13 @@
+print('if-else statements')
+a = 15
+if a%2 == 0:
+    print('Value is Even')
+else:
+    print('Value is Odd')
+
+print('if-else statement with user input()')
+a = int(input('Enter any integer number:'))
+if a%2 == 0:
+    print('Value is Even')
+else:
+    print('Value is Odd')
